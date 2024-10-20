1db6ec6d28c44fc224118257dc0f4540ade5ace8
steven mukama <stevenmukama1@gmail.com>
2024-10-20 23:01:02 +0200

    add more changes

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -9,6 +9,7 @@
 //   const textLinkClasses =
 //     'text-gray-500 hover:text-gray-900 active:text-gray-400';
 
+//   const textLinkClassesMob = 'block hover:text-white hover:bg-[#24330c] my-8 ';
 //   const navLinks = [
 //     { href: '#about', children: 'About Us' },
 //     { href: '#whatWedo', children: 'Services' },
@@ -49,7 +50,7 @@
 //             <ButtonLink
 //               href="#contactus"
 //               children={'Book a call now'}
-//               className="active:bg hidden rounded-xl px-5 py-3 text-white bg-[#daa51b] hover:bg-[#24330c] sm:flex"
+//               className="active:bg max-sm:hidden rounded-xl px-5 py-3 text-white bg-[#daa51b] hover:bg-[#24330c]"
 //             />
 //             <Disclosure.Button className="rounded-md p-2 text-gray-500 hover:bg-[#24330c] hover:text-white sm:hidden">
 //               {open ? (
@@ -59,20 +60,20 @@
 //               )}
 //             </Disclosure.Button>
 //           </div>
-//           <Disclosure.Panel className="space-y-1 px-4 sm:hidden">
+//           <Disclosure.Panel className=" px-8 sm:hidden">
 //             {navLinks.map((link, index) => (
 //               <Disclosure.Button
-//                 className="block"
 //                 as="a"
 //                 key={index}
 //                 href={link.href}
 //                 children={link.children}
+//                 className={textLinkClassesMob}
 //               />
 //             ))}
 //             <ButtonLink
 //               href="#contactus"
 //               children={'Book a call now'}
-//               className="active:bg block rounded-xl px-5 py-3 text-white bg-[#daa51b] hover:bg-[#24330c] sm:flex"
+//               className="active:bg block rounded-xl px-5 py-3 text-white bg-[#daa51b] hover:bg-[#24330c]"
 //             />
 //           </Disclosure.Panel>
 //         </>
@@ -96,11 +97,11 @@ function Nav() {
 
   const textLinkClassesMob = 'block hover:text-white hover:bg-[#24330c] my-8 ';
   const navLinks = [
-    { href: '#about', children: 'About Us' },
-    { href: '#whatWedo', children: 'Services' },
-    { href: '#reviews', children: 'Reviews' },
-    { href: '#team', children: 'Our Team' },
-    { href: '#contactus', children: 'Contact us' },
+    { href: '#about', label: 'About Us' },
+    { href: '#whatWedo', label: 'Services' },
+    { href: '#reviews', label: 'Reviews' },
+    { href: '#team', label: 'Our Team' },
+    { href: '#contactus', label: 'Contact us' },
   ];
 
   return (
@@ -109,34 +110,31 @@ function Nav() {
         <>
           <div className="flex items-center justify-between px-8 pt-3 xl:m-auto">
             <div className="flex">
-              <ButtonLink
-                href="#"
-                children={
-                  <Image
-                    src={logo}
-                    alt="Logo"
-                    priority={false}
-                    placeholder="empty"
-                    className="w-[9rem]"
-                  />
-                }
-              />
+              <ButtonLink href="#">
+                <Image
+                  src={logo}
+                  alt="Logo"
+                  priority={false}
+                  placeholder="empty"
+                  className="w-[9rem]"
+                />
+              </ButtonLink>
               <div className="ml-4 hidden items-center space-x-4 pl-8 sm:ml-6 sm:flex lg:ml-8 lg:space-x-8">
                 {navLinks.map((link, index) => (
                   <ButtonLink
                     key={index}
                     href={link.href}
-                    children={link.children}
-                    className={textLinkClasses}
-                  />
+                    className={textLinkClasses}>
+                    {link.label}
+                  </ButtonLink>
                 ))}
               </div>
             </div>
             <ButtonLink
               href="#contactus"
-              children={'Book a call now'}
-              className="active:bg max-sm:hidden rounded-xl px-5 py-3 text-white bg-[#daa51b] hover:bg-[#24330c]"
-            />
+              className="active:bg max-sm:hidden rounded-xl px-5 py-3 text-white bg-[#daa51b] hover:bg-[#24330c]">
+              Book a call now
+            </ButtonLink>
             <Disclosure.Button className="rounded-md p-2 text-gray-500 hover:bg-[#24330c] hover:text-white sm:hidden">
               {open ? (
                 <XMarkIcon className="block h-6 w-6" />
@@ -145,21 +143,21 @@ function Nav() {
               )}
             </Disclosure.Button>
           </div>
-          <Disclosure.Panel className=" px-8 sm:hidden">
+          <Disclosure.Panel className="px-8 sm:hidden">
             {navLinks.map((link, index) => (
               <Disclosure.Button
                 as="a"
                 key={index}
                 href={link.href}
-                children={link.children}
-                className={textLinkClassesMob}
-              />
+                className={textLinkClassesMob}>
+                {link.label}
+              </Disclosure.Button>
             ))}
             <ButtonLink
               href="#contactus"
-              children={'Book a call now'}
-              className="active:bg block rounded-xl px-5 py-3 text-white bg-[#daa51b] hover:bg-[#24330c]"
-            />
+              className="active:bg block rounded-xl px-5 py-3 text-white bg-[#daa51b] hover:bg-[#24330c]">
+              Book a call now
+            </ButtonLink>
           </Disclosure.Panel>
         </>
       )}
