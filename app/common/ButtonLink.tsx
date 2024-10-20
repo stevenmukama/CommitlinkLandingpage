957a8994c6cd9835dafe68dd032ffd37bc5519ab
steven mukama <stevenmukama1@gmail.com>
2024-10-20 17:10:58 +0200

    add more changes

--- app/common/ButtonLink.tsx
+++ app/common/ButtonLink.tsx
@@ -1,23 +1,39 @@
+// export interface ButtonLinkProps {
+//     href: string;
+//     children?: React.ReactNode;
+//     className?: string;
+//     target?: string;
+//     logo?: string;
+//     upperText?: string;
+//     lowerText?: string;
+//   }
+
+//   function ButtonLink({ href, children, className, target }: ButtonLinkProps) {
+//     return (
+//       <a
+//         href={href}
+//         children={children}
+//         className={className}
+//         target={target}
+//       ></a>
+//     );
+//   }
+
+//   export default ButtonLink;
+
 export interface ButtonLinkProps {
-    href: string;
-    children?: React.ReactNode;
-    className?: string;
-    target?: string;
-    logo?: string;
-    upperText?: string;
-    lowerText?: string;
-  }
-  
-  function ButtonLink({ href, children, className, target }: ButtonLinkProps) {
-    return (
-      <a
-        href={href}
-        children={children}
-        className={className}
-        target={target}
-      ></a>
-    );
-  }
-  
-  export default ButtonLink;
-  
+  href: string;
+  children?: React.ReactNode;
+  className?: string;
+  target?: string;
+}
+
+function ButtonLink({ href, children, className, target }: ButtonLinkProps) {
+  return (
+    <a href={href} className={className} target={target}>
+      {children}
+    </a>
+  );
+}
+
+export default ButtonLink;
